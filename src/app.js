5b4017b335bf184b2d3a7c6020a7e489aa45e1b9
Elze <elze.kilaite@mif.stud.vu.lt>
2022-02-21 23:49:50 +0200

    basic port implementation was changed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
 
+const port =
+  process.env.NODE_ENV !== "production"
+    ? process.env.LOCAL_PORT
+    : process.env.PORT;
+
+// routes
 app.get("", async (req, res) => {
   res.end("Hello World");
 });
