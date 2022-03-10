a19fe0c1352aeef641675ae5ba30af9f24d1627f
Elze <elze.kilaite@mif.stud.vu.lt>
2022-03-10 02:39:15 +0200

    config file optimizes server.js code

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p4km3.mongodb.net/Store`;
+
+async function main() {
+  //console.log("Connecting to database: " + connectionString);
+  await mongoose.connect(connectionString);
+  console.log("Connection to database was successful");
+}
+
+module.exports.connect = () => {
+  main().catch((err) => console.log(err));
+};
